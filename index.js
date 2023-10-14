5ea8668041071492fa7f5a87377727c9b8d4ae1c
Oscar <oscarlh24@hotmail.com>
2023-10-14 02:38:51 +0100

    Initialise Socket.IO

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 import path from 'node:path';
+import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 
 import express from 'express';
 import favicon from 'serve-favicon';
 
+import { Server } from 'socket.io';
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static('public'));
 
-app.listen(1337, () => {
+io.on('connection', (socket) => {
+    console.log('a user connected');
+})
+
+server.listen(1337, () => {
     console.log('Listening on port 1337');
 });
 
