0177ff7c137ebcfcfdb66246c75e92aaba3af66d
Oscar <oscarlh24@hotmail.com>
2023-10-28 20:14:35 +0100

    Basic movement

--- src/public/script.js
+++ src/public/script.js
@@ -1,6 +1,7 @@
 const socket = io();
 
 const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
@@ -9,38 +10,53 @@ onresize = () => {
     canvas.height = window.innerHeight;
 };
 
+let holdW = false;
+let holdA = false;
+let holdS = false;
+let holdD = false;
+
 addEventListener('keydown', event => {
-    if (!event.repeat) {
-        switch (event.code) {
-            case 'KeyW': console.log('W pressed'); break;
-            case 'KeyA': console.log('A pressed'); break;
-            case 'KeyS': console.log('S pressed'); break;
-            case 'KeyD': console.log('D pressed'); break;
-        }
+    if (event.repeat) return;
+
+    switch (event.code) {
+        case 'KeyW': holdW = true; break;
+        case 'KeyA': holdA = true; break;
+        case 'KeyS': holdS = true; break;
+        case 'KeyD': holdD = true; break;
     }
 });
 
 addEventListener('keyup', event => {
     switch (event.code) {
-        case 'KeyW': console.log('W released'); break;
-        case 'KeyA': console.log('A released'); break;
-        case 'KeyS': console.log('S released'); break;
-        case 'KeyD': console.log('D released'); break;
+        case 'KeyW': holdW = false; break;
+        case 'KeyA': holdA = false; break;
+        case 'KeyS': holdS = false; break;
+        case 'KeyD': holdD = false; break;
     }
 });
 
-const context = canvas.getContext('2d');
-context.fillStyle = 'rgb(255, 0, 255)';
+let playerPositionX = canvas.width / 2;
+let playerPositionY = canvas.height / 2;
+const PLAYER_SPEED = 200;
+const PLAYER_SIZE = 25;
+const PLAYER_COLOUR = 'rgb(255, 0, 0)';
 
 let prev = 0;
-let delta = 0;
+let deltaTime = 0;
 
 function tick(t) {
-    delta = t - prev;
+    deltaTime = (t - prev) / 1000;
     prev = t;
 
+    if (holdW) playerPositionY -= PLAYER_SPEED * deltaTime;
+    if (holdD) playerPositionX += PLAYER_SPEED * deltaTime;
+    if (holdS) playerPositionY += PLAYER_SPEED * deltaTime;
+    if (holdA) playerPositionX -= PLAYER_SPEED * deltaTime;
+
     context.clearRect(0, 0, canvas.width, canvas.height);
-    context.fillRect(t / 10, t / 10, 25, 25);
+
+    context.fillStyle = PLAYER_COLOUR;
+    context.fillRect(playerPositionX - PLAYER_SIZE / 2, playerPositionY - PLAYER_SIZE / 2, PLAYER_SIZE, PLAYER_SIZE);
 
     requestAnimationFrame(tick);
 }
