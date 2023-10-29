a8faa2e16155042836d39977d70dc0aa66a7d7d3
Oscar <oscarlh24@hotmail.com>
2023-10-29 01:57:05 +0100

    First multiplayer

--- src/index.js
+++ src/index.js
@@ -16,11 +16,41 @@ const io = new Server(server);
 app.use(favicon(join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(join(__dirname, 'public')));
 
+// TODO: Store session IDs
+// - https://socket.io/get-started/private-messaging-part-2/
+// - https://socket.io/docs/v4/client-options/#auth
+// - https://socket.io/how-to/deal-with-cookies
+
+const players = [];
+
 io.on('connection', (socket) => {
-    console.log('Someone connected');
+    // Send all existing players to new player
+    for (let player of players) {
+        socket.emit('player_connected', player.id, player.posX, player.posY, player.colourIndex);
+    }
+
+    // Broadcast new player to all other players
+    socket.broadcast.emit('player_connected', socket.id, null, null, 0);
+
+    players.push({ id: socket.id, posX: null, posY: null, colourIndex: 0 });
+
+    socket.on('player_move', (posX, posY) => {
+        const index = players.findIndex(player => player.id === socket.id);
+        players[index].posX = posX;
+        players[index].posY = posY;
+        socket.broadcast.emit('player_move', socket.id, posX, posY);
+    });
+
+    socket.on('player_change_colour', (colourIndex) => {
+        const index = players.findIndex(player => player.id === socket.id);
+        players[index].colourIndex = colourIndex;
+        socket.broadcast.emit('player_change_colour', socket.id, colourIndex);
+    });
 
     socket.on('disconnect', () => {
-        console.log('Someone disconnected');
+        const index = players.findIndex(player => player.id === socket.id);
+        players.splice(index, 1);
+        socket.broadcast.emit('player_disconnected', socket.id);
     })
 })
 
--- src/public/script.js
+++ src/public/script.js
@@ -38,6 +38,7 @@ addEventListener('keydown', event => {
         case 'Space': {
             ++playerColourIndex;
             if (playerColourIndex >= PLAYER_COLOURS.length) playerColourIndex = 0;
+            socket.emit('player_change_colour', playerColourIndex);
             break;
         }
     }
@@ -52,10 +53,37 @@ addEventListener('keyup', event => {
     }
 });
 
+const otherPlayers = [];
+
+socket.on('player_connected', (id, posX, posY, colourIndex) => {
+    otherPlayers.push({ id, posX, posY, colourIndex });
+});
+
+socket.on('player_move', (id, posX, posY) => {
+    const index = otherPlayers.findIndex(player => player.id === id);
+    otherPlayers[index].posX = posX;
+    otherPlayers[index].posY = posY;
+});
+
+socket.on('player_change_colour', (id, colourIndex) => {
+    const index = otherPlayers.findIndex(player => player.id === id);
+    otherPlayers[index].colourIndex = colourIndex;
+});
+
+socket.on('player_disconnected', (id) => {
+    const index = otherPlayers.findIndex(player => player.id === id);
+    otherPlayers.splice(index, 1);
+});
+
 const PLAYER_SPEED = 200;
 const PLAYER_SIZE = 25;
+let playerPreviousX = null;
+let playerPreviousY = null;
 let playerPositionX = Math.random() * canvas.width;
 let playerPositionY = Math.random() * canvas.height;
+
+// Set the player's initial position on the server
+socket.emit('player_move', playerPositionX, playerPositionY);
 
 let prev = 0;
 let deltaTime = 0;
@@ -64,10 +92,16 @@ function tick(t) {
     deltaTime = (t - prev) / 1000;
     prev = t;
 
+    playerPreviousX = playerPositionX;
+    playerPreviousY = playerPositionY;
+
     if (holdW) playerPositionY -= PLAYER_SPEED * deltaTime;
     if (holdD) playerPositionX += PLAYER_SPEED * deltaTime;
     if (holdS) playerPositionY += PLAYER_SPEED * deltaTime;
     if (holdA) playerPositionX -= PLAYER_SPEED * deltaTime;
+
+    if (playerPositionX !== playerPreviousX || playerPositionY !== playerPreviousY)
+        socket.emit('player_move', playerPositionX, playerPositionY);
 
     if (playerPositionY - PLAYER_SIZE / 2 < 0)
         playerPositionY = 0 + PLAYER_SIZE / 2;
@@ -79,6 +113,18 @@ function tick(t) {
         playerPositionX = 0 + PLAYER_SIZE / 2;
 
     context.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (player of otherPlayers) {
+        if (player.posX === null) continue;
+
+        context.fillStyle = PLAYER_COLOURS[player.colourIndex];
+        context.fillRect(
+            player.posX - PLAYER_SIZE / 2,
+            player.posY - PLAYER_SIZE / 2,
+            PLAYER_SIZE,
+            PLAYER_SIZE
+        );
+    }
 
     context.fillStyle = PLAYER_COLOURS[playerColourIndex];
     context.fillRect(
