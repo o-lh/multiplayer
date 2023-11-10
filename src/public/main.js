e99609fb148d377c4d9b43039f5f2158b1a805f3
Oscar <oscarlh24@hotmail.com>
2023-11-10 03:03:50 +0000

    Mousedown event, function renaming, canvas world size corrected

--- src/public/main.js
+++ src/public/main.js
@@ -7,19 +7,26 @@ class Point {
     }
 }
 
-const CANVAS_WORLD_SPACE_WIDTH = 10;
-const CANVAS_WORLD_SPACE_HEIGHT = 10;
+const CANVAS_WORLD_SPACE_WIDTH = 20;
+const CANVAS_WORLD_SPACE_HEIGHT = 20;
 
-function toScreenSpacePoint(worldSpacePoint) {
+function worldSpacePointToScreenSpace(point) {
     return new Point(
-        canvas.width / 2 + worldSpacePoint.x * canvas.width / (2 * CANVAS_WORLD_SPACE_WIDTH),
-        canvas.height / 2 + worldSpacePoint.y * canvas.height / (2 * CANVAS_WORLD_SPACE_HEIGHT)
+        canvas.width / 2 + point.x * canvas.width / CANVAS_WORLD_SPACE_WIDTH,
+        canvas.height / 2 + point.y * canvas.height / CANVAS_WORLD_SPACE_HEIGHT
     );
 }
 
 // TODO: What about for a non-square canvas?
-function toScreenSpaceLength(worldSpaceLength) {
-    return worldSpaceLength * canvas.height / (2 * CANVAS_WORLD_SPACE_HEIGHT);
+function worldSpaceLengthToScreenSpace(length) {
+    return length * canvas.height / CANVAS_WORLD_SPACE_HEIGHT;
+}
+
+function screenSpacePointToWorldSpace(point) {
+    return new Point(
+        point.x / canvas.width * CANVAS_WORLD_SPACE_WIDTH - CANVAS_WORLD_SPACE_WIDTH / 2,
+        point.y / canvas.height * CANVAS_WORLD_SPACE_HEIGHT - CANVAS_WORLD_SPACE_HEIGHT / 2
+    );
 }
 
 const canvas = document.getElementById('canvas');
@@ -29,15 +36,15 @@ canvas.width = smallerDimension;
 canvas.height = smallerDimension;
 
 const PLAYER_SIZE = 0.5;
-let playerSizeScreenSpace = toScreenSpaceLength(PLAYER_SIZE);
+let playerSizeScreenSpace = worldSpaceLengthToScreenSpace(PLAYER_SIZE);
 
-onresize = () => {
+addEventListener('resize', _ => {
     smallerDimension = innerWidth > innerHeight ? innerHeight : innerWidth;
     canvas.width = smallerDimension;
     canvas.height = smallerDimension;
 
-    playerSizeScreenSpace = toScreenSpaceLength(PLAYER_SIZE);
-};
+    playerSizeScreenSpace = worldSpaceLengthToScreenSpace(PLAYER_SIZE);
+});
 
 let holdW = false;
 let holdA = false;
@@ -82,6 +89,15 @@ addEventListener('keyup', event => {
     }
 });
 
+// TODO: Make canvas fill the entire window
+canvas.addEventListener('mousedown', event => {
+    if (event.button !== 0) return;
+
+    console.log(screenSpacePointToWorldSpace(
+        new Point(event.x - canvas.offsetLeft, event.y - canvas.offsetTop)
+    ));
+});
+
 const otherPlayers = [];
 
 socket.on('player_connected', (id, posX, posY, colourIndex) => {
@@ -107,8 +123,8 @@ socket.on('player_disconnected', (id) => {
 const PLAYER_SPEED = 4;
 let playerPreviousX = null;
 let playerPreviousY = null;
-let playerPositionX = (Math.random() * 2 * CANVAS_WORLD_SPACE_WIDTH) - CANVAS_WORLD_SPACE_WIDTH;
-let playerPositionY = (Math.random() * 2 * CANVAS_WORLD_SPACE_HEIGHT) - CANVAS_WORLD_SPACE_HEIGHT;
+let playerPositionX = (Math.random() * CANVAS_WORLD_SPACE_WIDTH) - CANVAS_WORLD_SPACE_WIDTH / 2;
+let playerPositionY = (Math.random() * CANVAS_WORLD_SPACE_HEIGHT) - CANVAS_WORLD_SPACE_HEIGHT / 2;
 
 // Set the player's initial position on the server
 socket.emit('player_move', playerPositionX, playerPositionY);
@@ -131,21 +147,21 @@ function tick(t) {
     if (playerPositionX !== playerPreviousX || playerPositionY !== playerPreviousY)
         socket.emit('player_move', playerPositionX, playerPositionY);
 
-    if (playerPositionY - PLAYER_SIZE / 2 < -CANVAS_WORLD_SPACE_HEIGHT)
-        playerPositionY = -CANVAS_WORLD_SPACE_HEIGHT + PLAYER_SIZE / 2;
-    if (playerPositionX + PLAYER_SIZE / 2 > CANVAS_WORLD_SPACE_WIDTH)
-        playerPositionX = CANVAS_WORLD_SPACE_WIDTH - PLAYER_SIZE / 2;
-    if (playerPositionY + PLAYER_SIZE / 2 > CANVAS_WORLD_SPACE_HEIGHT)
-        playerPositionY = CANVAS_WORLD_SPACE_HEIGHT - PLAYER_SIZE / 2;
-    if (playerPositionX - PLAYER_SIZE / 2 < -CANVAS_WORLD_SPACE_WIDTH)
-        playerPositionX = -CANVAS_WORLD_SPACE_WIDTH + PLAYER_SIZE / 2;
+    if (playerPositionY - PLAYER_SIZE / 2 < -CANVAS_WORLD_SPACE_HEIGHT / 2)
+        playerPositionY = -CANVAS_WORLD_SPACE_HEIGHT / 2 + PLAYER_SIZE / 2;
+    if (playerPositionX + PLAYER_SIZE / 2 > CANVAS_WORLD_SPACE_WIDTH / 2)
+        playerPositionX = CANVAS_WORLD_SPACE_WIDTH / 2 - PLAYER_SIZE / 2;
+    if (playerPositionY + PLAYER_SIZE / 2 > CANVAS_WORLD_SPACE_HEIGHT / 2)
+        playerPositionY = CANVAS_WORLD_SPACE_HEIGHT / 2 - PLAYER_SIZE / 2;
+    if (playerPositionX - PLAYER_SIZE / 2 < -CANVAS_WORLD_SPACE_WIDTH / 2)
+        playerPositionX = -CANVAS_WORLD_SPACE_WIDTH / 2 + PLAYER_SIZE / 2;
 
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     for (const player of otherPlayers) {
         if (player.posX === null) continue;
 
-        const playerPos = toScreenSpacePoint(new Point(player.posX, player.posY));
+        const playerPos = worldSpacePointToScreenSpace(new Point(player.posX, player.posY));
 
         context.fillStyle = PLAYER_COLOURS[player.colourIndex];
         context.fillRect(
@@ -156,7 +172,7 @@ function tick(t) {
         );
     }
 
-    const playerPos = toScreenSpacePoint(new Point(playerPositionX, playerPositionY));
+    const playerPos = worldSpacePointToScreenSpace(new Point(playerPositionX, playerPositionY));
 
     context.fillStyle = PLAYER_COLOURS[playerColourIndex];
     context.fillRect(
