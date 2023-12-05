3e7412d5e787ee1d6d4af053bb11185f1be6330b
Oscar <oscarlh24@hotmail.com>
2023-12-05 03:23:17 +0000

    Move socket events to Network class

--- src/public/scripts/game.js
+++ src/public/scripts/game.js
@@ -3,7 +3,6 @@ import { Entity } from './entity.js';
 import { Input } from './input.js';
 import { Network } from './network.js';
 import { Player } from './components/player.js';
-import { Projectile } from "./components/projectile.js";
 import { Time } from './time.js';
 import { Vector2 } from "./vector2.js";
 
@@ -78,70 +77,10 @@ export class Game {
             Game.playerRadiusScreenSpace = Game.worldSpaceLengthToScreenSpace(Game.PLAYER_RADIUS);
         });
 
+        Network.init();
         this.#updateInput = Input.init();
 
-        Network.socket.on('player_connected', newPlayer => {
-            Game.otherPlayers.push(newPlayer);
-        });
-
-        Network.socket.on('player_move', (id, position) => {
-            const index = Game.otherPlayers.findIndex(player => player.id === id);
-            Game.otherPlayers[index].position = position;
-        });
-
-        Network.socket.on('create_entity', serializedEntity => {
-            serializedEntity = JSON.parse(serializedEntity);
-
-            const entity = Game.addEntity();
-
-            // TODO: Move these... somewhere
-            // TODO: But deserializeProperties requires entity in scope to add it to components
-            const lookup = { Vector2: Vector2, Projectile: Projectile };
-
-            function deserializeProperties(serializedObject, deserializedObject) {
-                for (const property in serializedObject) {
-                    if (typeof serializedObject[property] === 'object') {
-                        if (Array.isArray(serializedObject[property])) {
-                            deserializedObject[property] = [];
-                        } else {
-                            deserializedObject[property] =
-                                new lookup[serializedObject[property].constructorName]();
-
-                            // TODO: This will need to be changed if any other non-component classes are added
-                            if (deserializedObject[property].constructorName !== 'Vector2') {
-                                deserializedObject[property].entity = entity;
-                            }
-                        }
-
-                        deserializeProperties(
-                            serializedObject[property],
-                            deserializedObject[property]
-                        );
-                    } else {
-                        deserializedObject[property] = serializedObject[property];
-                    }
-                }
-            }
-
-            deserializeProperties(serializedEntity, entity);
-        });
-
-        Network.socket.on('projectile_hit', (projectileID, targetID) => {
-            Game.entities[Game.entities.findIndex(x => x.id === projectileID)].destroyed = true;
-
-            if (targetID === Network.socket.id) {
-                ++Game.player.getComponent(Player).hitsTaken;
-            } else {
-                const index = Game.otherPlayers.findIndex(player => player.id === targetID);
-                ++Game.otherPlayers[index].hitsTaken;
-            }
-        });
-
-        Network.socket.on('player_disconnected', id => {
-            const index = Game.otherPlayers.findIndex(player => player.id === id);
-            Game.otherPlayers.splice(index, 1);
-        });
-
+        // TODO: Where should this belong?
         Network.socket.on('connected', () => {
             this.player = this.addEntity();
             this.player.addComponent(Player).init();
--- src/public/scripts/network.js
+++ src/public/scripts/network.js
@@ -1,3 +1,73 @@
+import { Game } from "./game.js";
+import { Player } from "./components/player.js";
+import { Projectile } from "./components/projectile.js";
+import { Vector2 } from "./vector2.js";
+
 export class Network {
     static socket = io();
+
+    // TODO: Separate into #componentConstructors and #objectConstructors
+    static #constructors = { Vector2: Vector2, Projectile: Projectile };
+
+    // TODO: Singleton
+    static init() {
+        Network.socket.on('player_connected', newPlayer => {
+            Game.otherPlayers.push(newPlayer);
+        });
+
+        Network.socket.on('player_move', (id, position) => {
+            const index = Game.otherPlayers.findIndex(player => player.id === id);
+            Game.otherPlayers[index].position = position;
+        });
+
+        Network.socket.on('create_entity', serializedEntity => {
+            serializedEntity = JSON.parse(serializedEntity);
+
+            const entity = Game.addEntity();
+
+            // TODO: Move this outside of this function (entity needs to be in scope)
+            function deserializeProperties(serializedObject, deserializedObject) {
+                for (const property in serializedObject) {
+                    if (typeof serializedObject[property] === 'object') {
+                        if (Array.isArray(serializedObject[property])) {
+                            deserializedObject[property] = [];
+                        } else {
+                            deserializedObject[property] =
+                                new Network.#constructors[serializedObject[property].constructorName]();
+
+                            // TODO: This will need to be changed if any other non-component classes are added
+                            if (deserializedObject[property].constructorName !== 'Vector2') {
+                                deserializedObject[property].entity = entity;
+                            }
+                        }
+
+                        deserializeProperties(
+                            serializedObject[property],
+                            deserializedObject[property]
+                        );
+                    } else {
+                        deserializedObject[property] = serializedObject[property];
+                    }
+                }
+            }
+
+            deserializeProperties(serializedEntity, entity);
+        });
+
+        Network.socket.on('projectile_hit', (projectileID, targetID) => {
+            Game.entities[Game.entities.findIndex(x => x.id === projectileID)].destroyed = true;
+
+            if (targetID === Network.socket.id) {
+                ++Game.player.getComponent(Player).hitsTaken;
+            } else {
+                const index = Game.otherPlayers.findIndex(player => player.id === targetID);
+                ++Game.otherPlayers[index].hitsTaken;
+            }
+        });
+
+        Network.socket.on('player_disconnected', id => {
+            const index = Game.otherPlayers.findIndex(player => player.id === id);
+            Game.otherPlayers.splice(index, 1);
+        });
+    }
 }
