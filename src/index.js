8e7ca5c8bfc60bc44000d52e3842725d511654fa
Oscar <oscarlh24@hotmail.com>
2023-12-09 02:23:12 +0000

    Remove server update loop for now

--- src/index.js
+++ src/index.js
@@ -76,32 +76,3 @@ io.on('connection', (socket) => {
 server.listen(1337, () => {
     console.log('Listening on port 1337');
 });
-
-let count = 0;
-
-function update() {
-    const origin = new Vector2(Math.random() * 10 - 5, Math.random() * 10 - 5);
-    const direction = new Vector2(Math.random() * 2 - 1, Math.random() * 2 - 1).normalized;
-
-    for (const socket of sockets) {
-        socket.emit('create_entity', `{
-"id":"server-${count}",
-"position":{"constructorName":"Vector2","x":0,"y":0},
-"components":[
-{
-"constructorName":"Projectile",
-"owner":"server",
-"origin":{"constructorName":"Vector2","x":${origin.x},"y":${origin.y}},
-"direction":{"constructorName":"Vector2","x":${direction.x},"y":${direction.y}},
-"speed":50,
-"head":{"constructorName":"Vector2","x":${origin.x},"y":${origin.y}},
-"tail":{"constructorName":"Vector2","x":${origin.x},"y":${origin.y}}
-}
-]
-}`);
-    }
-
-    ++count;
-}
-
-setInterval(update, 1000);
